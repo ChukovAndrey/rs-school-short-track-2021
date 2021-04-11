56a1299dffa13e2d48c4af137b7682419874775c
ChukovAndrey <chukov.info@gmail.com>
2021-04-11 20:59:17 +0300

    feat: add basic solution

--- src/10-find-index.js
+++ src/10-find-index.js
@@ -11,8 +11,13 @@
  * For ([1, 2, 3], 2) should return 1
  *
  */
-function findIndex(/* array, value */) {
-  throw new Error('Not implemented');
+function findIndex(array, value) {
+  for (let i = 0; i < array.length; i += 1) {
+    if (array[i] === value) {
+      return i;
+    }
+  }
+  return null;
 }
 
 module.exports = findIndex;
